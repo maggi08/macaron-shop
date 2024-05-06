6760a854407aa2b522f2ba9f9bb402aefe89460f
Mako <maggi08mus@gmail.com>
2024-05-06 21:31:38 +0500

    footer docs

--- src/components/layouts/MFooter.vue
+++ src/components/layouts/MFooter.vue
@@ -1,9 +1,76 @@
 <template>
-  <footer>footer</footer>
+  <footer class="footer">
+    <div class="footer-content container">
+      <div class="">
+        <div class="">
+          <div class="">
+            <img src="./icons/footer-icon1.svg" alt="" />
+            <p>Готовим вручную и с любовью</p>
+          </div>
+          <div class="">
+            <img src="./icons/footer-icon2.svg" alt="" />
+            <p>Доставим в день заказа</p>
+          </div>
+          <div class="">
+            <img src="./icons/footer-icon3.svg" alt="" />
+            <p>100% миндальная мукаи натуральные ингредиенты</p>
+          </div>
+        </div>
+        <div class="">
+          © 2021 Макароншоп ООО "Квантум", Санкт-Петербург, улица Маршала Тухачевского, дом 22
+        </div>
+      </div>
+      <div class="">
+        <div class="">
+          <ul>
+            <li>Информация</li>
+            <li>О компании</li>
+            <li>Гарантии вкуса и свежести</li>
+            <li>Доставка и оплата</li>
+            <li>Контакты</li>
+          </ul>
+        </div>
+        <div class="">
+          <ul>
+            <li>Каталог</li>
+            <li>Каталог десертов</li>
+            <li>Готовые наборы</li>
+            <li>Собрать свой набор</li>
+            <li>Акции</li>
+          </ul>
+        </div>
+        <div class="">
+          <ul>
+            <li>ДЛЯ БИЗНЕСА</li>
+            <li>Корпоративные подарки</li>
+            <li>Для юридических лиц</li>
+            <li>Оповикам</li>
+          </ul>
+        </div>
+      </div>
+      <div class="">
+        <div class="">
+          <p>+7 (812) 309 82 88</p>
+          <p>с 9:00 до 21:00</p>
+        </div>
+        <div class="">
+          <img src="./icons/footer-social-inst.svg" alt="" />
+          <img src="./icons/footer-social-fb.svg" alt="" />
+          <img src="./icons/footer-social-vk.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+.footer {
+  background: #ebedee;
+  padding-top: 56px;
+  padding-bottom: 64px;
+}
+</style>
